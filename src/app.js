0fd8390bb0d40b7ef26a7bdad59a09d132888270
obotha <khyper234@gmail.com>
2021-07-09 19:00:52 +0300

    Refactor the code.

--- src/app.js
+++ src/app.js
@@ -1,25 +1,9 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
-
-const User = require('./user/User');
 
 const app = express();
 app.use(express.json());
 
-app.post('/api/1.0/users', (req, res) => {
-  bcrypt.hash(req.body.password, 10).then((hash) => {
-    // const user = Object.assign({}, req.body, { password: hash });
-    const user = {
-      ...req.body,
-      password: hash,
-    };
-
-    User.create(user).then(() => {
-      return res.send({
-        message: 'User created.',
-      });
-    });
-  });
-});
+const userRouter = require('./user/userRouter');
+app.use(userRouter);
 
 module.exports = app;
---
+++ src/user/userRouter.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+
+const User = require('./User');
+
+const router = express.Router();
+
+router.post('/api/1.0/users', async (req, res) => {
+  const hash = await bcrypt.hash(req.body.password, 10);
+
+  const user = {
+    ...req.body,
+    password: hash,
+  };
+
+  await User.create(user);
+
+  return res.send({
+    message: 'User created.',
+  });
+});
+
+module.exports = router;
