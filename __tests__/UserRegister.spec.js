03c5eea01b0ebf3a2baf4ef2841a6655ed4313d1
obotha <khyper234@gmail.com>
2021-07-09 11:01:26 +0300

    write first successful test.

---
+++ __tests__/UserRegister.spec.js
@@ -0,0 +1,17 @@
+const request = require('supertest');
+
+const app = require('../app');
+
+it('returns 200 OK when signup request is valid.', (done) => {
+  request(app)
+    .post('/api/1.0/users')
+    .send({
+      username: 'user1',
+      email: 'user1@mail.com',
+      password: 'P4ssword',
+    })
+    .then((response) => {
+      expect(response.status).toBe(200);
+      done();
+    });
+});
