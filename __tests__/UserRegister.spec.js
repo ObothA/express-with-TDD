4f0620b834d36ef42ea84daf27d4846ec2ef001e
obotha <khyper234@gmail.com>
2021-07-11 11:05:03 +0300

    Fix promise rejection issue.

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -3,7 +3,6 @@ const request = require('supertest');
 const app = require('../src/app');
 const User = require('../src/user/User');
 const sequelize = require('../src/config/database');
-const { describe } = require('../src/user/User');
 
 beforeAll(() => {
   return sequelize.sync();
